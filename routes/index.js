0d919f77209e3aa8da03d7c32ad868fb5fee6f0e
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-07-26 15:35:31 -0400

    made a user

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
+const userList = [{
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  email: "jd@gmail.com"
+}];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+router.get("/get-users", function (req, res, next) {
+  res.json(userList)
+})
 
 router.post("/post-message", function (req, res, next) {
   const clientMessage = req.body.clientMessage;
